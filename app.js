8ba5b955f88250ea8251ef2a707b5d61443214bc
Dawn Monroe <deetriplett@gmail.com>
2017-02-03 17:49:12 -0500

    added the plan

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-//Problem
+//Problem: Look for users badge count, js points from web browser
+//Solution:use node.js to perfrom profile lookups and server our template by http
 
-//Solution
+//Create web server
+
+//Handle HTTP route GET/ and POST/ ie HOME
+  //if url == "/" && GET 
+    //Show URL
+  //if url == "/" && POST
+    //redirect to /:username
+
+//Handle HTTP rout GET /:username ie dawntriplett
+  //if url == "/...."
+     //get JSON from Treehouse 
+        // on 'end'
+          //show profile
+        //on 'error'
+          //show error
+
+//function that handles the reading of files and merge in value
+  //read from file and get string
+  //merge values into string
+  
+
